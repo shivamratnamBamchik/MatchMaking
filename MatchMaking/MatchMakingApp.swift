d63921a56db2a45a855fe0bbd9464517adac3748
shivamratnamBamchik <shivam@bamchik.com>
2024-09-06 17:35:52 +0530

    code updated

--- MatchMaking/MatchMakingApp.swift
+++ MatchMaking/MatchMakingApp.swift
@@ -2,16 +2,18 @@
 //  MatchMakingApp.swift
 //  MatchMaking
 //
-//  Created by Shivam Ratnam on 06/09/24.
+//  Created by Shivam Ratnam on 05/09/24.
 //
 
 import SwiftUI
 
 @main
 struct MatchMakingApp: App {
+    let persistenceController = PersistenceController.shared
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
