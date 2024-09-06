d2006ef598ea07905be51d52ddeed91e2812f1e6
shivamratnamBamchik <shivam@bamchik.com>
2024-09-06 17:25:47 +0530

    Initial Commit

---
+++ MatchMaking/MatchMakingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMakingApp.swift
+//  MatchMaking
+//
+//  Created by Shivam Ratnam on 06/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MatchMakingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
